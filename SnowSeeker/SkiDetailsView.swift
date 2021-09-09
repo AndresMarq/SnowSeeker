e3dbef9eab8c77c4481b76169e0730deb915a33e
AndresMarq <70426525+AndresMarq@users.noreply.github.com>
2021-09-08 19:57:10 -0600

    Detail Views created, Day 97 of course completed

---
+++ SnowSeeker/SkiDetailsView.swift
@@ -0,0 +1,25 @@
+//
+//  SkiDetailsView.swift
+//  SnowSeeker
+//
+//  Created by Andres Marquez on 2021-09-08.
+//
+
+import SwiftUI
+
+struct SkiDetailsView: View {
+    let resort: Resort
+    
+    var body: some View {
+        VStack {
+            Text("Elevation: \(resort.elevation)m")
+            Text("Snow: \(resort.snowDepth)cm")
+        }
+    }
+}
+
+struct SkiDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SkiDetailsView(resort: Resort.example)
+    }
+}
